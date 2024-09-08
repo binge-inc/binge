4052b17fb874d398fd9ef8405120df111d53c52f
Binge Watch Incorporated <peterarbeitsloser1@gmail.com>
2024-09-08 13:00:28 +0200

    Set initial version to 0.1.0

--- src/binge/Build.java
+++ src/binge/Build.java
@@ -1,8 +1,8 @@
 package binge;
 
 public class Build {
-    private static final int VERSION_MAJOR = 1;
-    private static final int VERSION_MINOR = 0;
+    private static final int VERSION_MAJOR = 0;
+    private static final int VERSION_MINOR = 1;
     private static final int VERSION_PATCH = 0;
 
     public static String getFullVersionString() {
